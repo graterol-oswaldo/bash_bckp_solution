eec70481668673524d563dfa38dbd97976baa50a
graterol-oswaldo <oswaldo.graterol@gmail.com>
2017-08-15 02:05:50 -0400

    Update README.rb

--- README.rb
+++ README.rb
@@ -1,4 +1,4 @@
-# Scripts de Respaldo para Directorios y Base de Datos  
+#	Scripts de Respaldo para Directorios y Base de Datos  
 
-## Una solucion compuestas por varios scripts que configurada correctamente permite gestionar el respaldo de los directorios y bases de datos (PostgreSQL y MySQL), contando ademas con la bondad de generar por cada ejecucion un registro de bitacora, el cual posteriormente podra ser remitido via correo electronico.
+	##	Una solucion compuestas por varios scripts que configurada correctamente permite gestionar el respaldo de los directorios y bases de datos (PostgreSQL y MySQL), contando ademas con la bondad de generar por cada ejecucion un registro de bitacora, el cual posteriormente podra ser remitido via correo electronico.
 
