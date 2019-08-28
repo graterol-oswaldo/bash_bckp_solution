d33567a9534ae00b54c83482faa7de641f44c9ce
graterol-oswaldo <oswaldo.graterol@gmail.com>
2019-08-28 17:28:10 -0400

    Update README.rb
    ajustes correspondientes al archivo readme del proyecto.

--- README.rb
+++ README.rb
@@ -1,22 +1,5 @@
 
-[<img src="http://www.siace.org.ve/images/slider/img1.png">](http://www.siace.org.ve)
----
-## Sistema Integral Administrativo de las Contralorías de Estado
----
-
-- [Entorno de Desarrollo](#Entorno-de-Desarrollo)  
-
-<a name="Entorno-de-Desarrollo"></a>
-## 1. Entorno de Desarrollo
-### 1.1 Programador
-* PhpStorm: Interfaz de Desarrollo para PHP, HTML y JavaScript.
-* MySQL Workbench: Herramienta visual de diseño de bases de datos que integra desarrollo de software, Administración de bases de datos, diseño de bases de datos, creación y mantenimiento para el sistema de base de datos MySQL.
-
-### 1.2 Servidor
-* Ubuntu Server 16.04.3 LTS 
-* MySQL 5.7 
-* apache2 2.4.18
-* git 2.14.1
+## Solución para efectuar Respaldos desde B
 
 # Scripts de Respaldo para Directorios y Base de Datos  
 ## Una solucion compuestas por varios scripts que configurada correctamente permite gestionar el respaldo de los directorios y bases de datos (PostgreSQL y MySQL), contando ademas con la bondad de generar por cada ejecucion un registro de bitacora, el cual posteriormente podra ser remitido via correo electronico.
